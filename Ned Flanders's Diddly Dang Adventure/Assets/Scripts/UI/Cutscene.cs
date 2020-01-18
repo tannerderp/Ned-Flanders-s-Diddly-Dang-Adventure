9b2c5c41e32a164c7120e25863fe60b614e13c3d
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2020-01-18 12:50:28 -0700

    intro cutscene and main menu stuff

---
+++ Ned Flanders's Diddly Dang Adventure/Assets/Scripts/UI/Cutscene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    public double time;
+    public double currentTime;
+
+    private int wait = 0;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wait++;
+        if (wait == 80)
+        {
+            time = GetComponent<VideoPlayer>().frameCount / 30 + 0.7;
+            Debug.Log(time);
+        }
+        if (wait > 80)
+        {
+            currentTime = GetComponent<VideoPlayer>().time;
+            if (currentTime > time - 0.1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
