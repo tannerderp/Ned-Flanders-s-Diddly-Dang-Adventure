afac47cebdcca91440c00d6130a849b7a56bb857
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2020-02-15 13:04:51 -0700

    camera and sidewalk and road
    I've been swamped with schoolwork this week, trying to get back on track

---
+++ Ned Flanders's Diddly Dang Adventure/Assets/Scripts/Levels/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] bool followY = false; //whether the camera will also move on the y axis or not
+
+    private float cameraY = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraY = 0;
+        if(followY == true)
+        {
+            cameraY = Player.transform.position.y;
+        }
+        transform.position = new Vector3(Player.transform.position.x, cameraY, -10);
+    }
+}
